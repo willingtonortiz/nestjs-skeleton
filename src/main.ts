9c6c834d7c968e9f30ad7e98c2365ae838c386fb
willingtonortiz <willingtonortizmaurtua@hotmail.com>
2022-07-01 15:56:36 -0500

    Remove default controller and services; Add environment variables files

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,17 @@
 import { NestFactory } from '@nestjs/core';
+import helmet from 'helmet';
 
 import { AppModule } from './app.module';
 
 async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule);
+
+  app.use(helmet());
+
+  // CORS configuration
+  app.enableCors({ origin: ['*'] });
+
   await app.listen(3000);
 }
+
 bootstrap();
